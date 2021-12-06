8fe1052830f8ab561813cb9c37a36a46f7c2b2fa
Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>
2021-12-06 18:49:03 -0300

    api: create project with some settings
    This commit adds the use of the docker, which helps in the development of the
    project. You can run this command to start the project:
    
    ```
    - docker-compose up
    ```
    
    With that, all project dependencies will be downloaded and installed inside the
    container. In addition, the nodemon package is used to refresh the project, and
    the eslint airbnb package is installed for the project to follow a pattern.

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+try {
+  mongoose.connect('mongodb://db:27017/crud-node-mongo-docker', {
+    useNewUrlParser: true,
+  });
+
+  console.log('MongoDB connected');
+} catch (error) {
+  console.log(error);
+}
+
+app.listen(3000, () => console.log('Server active on port 3000'));
