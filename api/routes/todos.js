cf9247d85abaecee468d6c523d579dafe630780c
Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>
2021-12-27 10:35:25 -0300

    api: apply rest api design patterns in endpoints

--- api/routes/todos.js
+++ api/routes/todos.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 const todosController = require('../controllers/todos');
 
-router.post('/new', todosController.createTodo);
-router.get('/list', todosController.listTodo);
-router.get('/list/:id', todosController.getTodo);
-router.delete('/delete/:id', todosController.deleteTodo);
+router.post('', todosController.createTodo);
+router.get('', todosController.listTodo);
+router.get('/:id', todosController.getTodo);
+router.delete('/:id', todosController.deleteTodo);
 
 module.exports = router;
